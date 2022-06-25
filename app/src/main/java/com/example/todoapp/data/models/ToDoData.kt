507ba13458326e399df106316cd0bd738798450b
minezeratul <619492300@qq.com>
2022-06-25 16:44:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/data/models/ToDoData.kt
@@ -0,0 +1,18 @@
+package com.example.todoapp.data.models
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "todo_table")
+//自动创建 writeToParcel() 和 createFromParcel()
+//类似Lombook AllConstructors
+@Parcelize
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+) : Parcelable
